2cfbf43956ed09becc742b72e1ec2c8d751940ce
Juan Heyns <juanheyns@gmail.com>
2015-03-20 16:40:50 +0200

    jrebel

--- jrebel.rb
+++ jrebel.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class JRebel < Formula
+class Jrebel < Formula
   homepage 'http://zeroturnaround.com/software/jrebel/'
   url 'https://dl.zeroturnaround.com/?token=f32d22dd4446d49574e03517f906da0eb5c0dc84'
   version '5.6.3a'
