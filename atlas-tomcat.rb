883dfef917f6491a957f2c0de08e26da8afedf80
Juan Heyns <juanheyns@gmail.com>
2015-03-20 16:19:47 +0200

    tomcat archive

--- atlas-tomcat.rb
+++ atlas-tomcat.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class AtlasTomcat < Formula
   homepage "http://tomcat.apache.org/"
-  url "http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.35/bin/apache-tomcat-7.0.35.tar.gz"
+  url "http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.35/bin/apache-tomcat-7.0.35.tar.gz"
   sha1 "c9f8d87a212091c33027f122aba90b78eb1c0a01"
 
   option "with-fulldocs", "Install full documentation locally"
 
   resource "fulldocs" do
-    url "http://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.35/bin/apache-tomcat-7.0.35-fulldocs.tar.gz"
+    url "http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.35/bin/apache-tomcat-7.0.35-fulldocs.tar.gz"
     version "7.0.35"
     sha1 "96852de4949a1765b1a5478c8c586220bfc16377"
   end
