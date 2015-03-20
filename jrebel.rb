f0d500ea4008d3c54603a3b71c8845cb400355c8
Juan Heyns <juanheyns@gmail.com>
2015-03-20 22:26:47 +0200

    updating jrebel

--- jrebel.rb
+++ jrebel.rb
@@ -5,10 +5,11 @@ class Jrebel < Formula
   url 'https://dl.zeroturnaround.com/?token=f32d22dd4446d49574e03517f906da0eb5c0dc84'
   version '5.6.3a'
   sha1 '61f2ec9f2a5b1bd8a92c300f30b3685b99a956cf'
+  keg_only "The scripts are only executed once when installing."
 
   def install
-    prefix.install Dir['*']
     rm_rf Dir['bin/*.cmd']
+    prefix.install Dir['*']
   end
 end
 
