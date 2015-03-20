967d1fa2cb98d769ae178747ed3ff30a01801859
Juan Heyns <juanheyns@gmail.com>
2015-03-20 14:31:20 +0200

    Adding sim.

---
+++ sim.rb
@@ -0,0 +1,10 @@
+class Sim < Formula
+  homepage "https://github.com/juanheyns/sim"
+  url "https://github.com/juanheyns/sim/archive/0.4.tar.gz"
+  version "0.4"
+  sha256 "74de59c135a4240f9050875af6f50517872d3226489cece79f8833ceda228611"
+
+  def install
+    bin.install "sim2"
+  end
+end
