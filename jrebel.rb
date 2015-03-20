29abbde4619c21c16dd4f8d6a0d98dea71383436
Juan Heyns <juanheyns@gmail.com>
2015-03-20 16:43:07 +0200

    jrebel

--- jrebel.rb
+++ jrebel.rb
@@ -4,7 +4,7 @@ class Jrebel < Formula
   homepage 'http://zeroturnaround.com/software/jrebel/'
   url 'https://dl.zeroturnaround.com/?token=f32d22dd4446d49574e03517f906da0eb5c0dc84'
   version '5.6.3a'
-  sha1 'e024b6ffe9fdebb424693d7fd76e3aa4575be3db'
+  sha1 '61f2ec9f2a5b1bd8a92c300f30b3685b99a956cf'
 
   def install
     prefix.install Dir['*']
