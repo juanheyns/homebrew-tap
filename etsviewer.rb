20b5c86d9458bec3853ec30b8eb833fba9ea205d
Juan Heyns <juanheyns@gmail.com>
2015-03-20 16:59:53 +0200

    etsviewer

--- etsviewer.rb
+++ etsviewer.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Etsviewer < Formula
   homepage 'http://www.enthuware.com/'
-  url 'http://enthuware.com/etsviewer/etsviewer.jar', using => NoUnzipCurlDownloadStrategy
+  url 'http://enthuware.com/etsviewer/etsviewer.jar', :using => NoUnzipCurlDownloadStrategy
   version '3.56'
   md5 '62840e1a26d3d01c7e1f5af941f39cff'
 
