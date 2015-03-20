b0629f2c4774f16a887dd2fa5281ccdb0a0af964
Juan Heyns <juanheyns@gmail.com>
2015-03-20 15:50:04 +0200

    fix

--- sim.rb
+++ sim.rb
@@ -2,9 +2,9 @@ class Sim < Formula
   homepage "https://github.com/juanheyns/sim"
   url "https://github.com/juanheyns/sim/archive/0.5.tar.gz"
   version "0.5"
-  sha256 "74de59c135a4240f9050875af6f50517872d3226489cece79f8833ceda228611"
+  sha256 "3269670617d1a7c203377eb1c1028b210e95a9a6b6a9732486979ea2e7c56e41"
 
   def install
-    bin.install "sim2"
+    bin.install "sim"
   end
 end
