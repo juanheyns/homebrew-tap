24b37a0bac51324d5b4c6b565f86782bba737c10
Juan Heyns <juan@heldcraft.com>
2026-04-27 22:56:56 -0400

    fix: remove obsolete formula

--- sim.rb
+++
@@ -1,9 +0,0 @@
-class Sim < Formula
-  homepage "https://github.com/juanheyns/sim"
-  url "https://github.com/juanheyns/sim/archive/0.5.tar.gz"
-  sha1 "e5173d3af494db13088c8da51855b3de31701f19"
-
-  def install
-    bin.install "sim"
-  end
-end
